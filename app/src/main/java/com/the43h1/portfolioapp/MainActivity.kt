864fb7edca71e8f516c27276da4b667975cef2ac
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2024-12-19 00:19:39 +0530

    Now Buttons working properly

--- app/src/main/java/com/the43h1/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/the43h1/portfolioapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.the43h1.portfolioapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,9 +11,23 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        
+        val BSkills = findViewById<Button>(R.id.SkillsB)
+
+        BSkills.setOnClickListener {
+            intent = Intent(this, SkillsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val BEdu = findViewById<Button>(R.id.EducationB)
+
+        BSkills.setOnClickListener {
+            intent = Intent(this, EducationActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
